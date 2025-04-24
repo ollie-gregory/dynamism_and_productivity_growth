61c1d7393b1d7f4b5aadda6f509a4bec81d97d35
Oliver Gregory <oliver@Olivers-Laptop.local>
2025-04-24 13:36:36 +0100

    clean compustat data

--- src/markup_estimation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-from scipy.optimize import minimize
-from statsmodels.regression.linear_model import OLS
-
-class ProductionFunctionEstimator:
-    
-class MarkupEstimator:
